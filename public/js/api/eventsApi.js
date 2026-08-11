f3112c8a57371640f11222aabc1adbb8e6bb7380
clinton-t3 <clintontran12@gmail.com>
2026-08-11 13:50:14 -0400

    Fix function related to API for getting events

--- public/js/api/eventsApi.js
+++ public/js/api/eventsApi.js
@@ -1,7 +1,40 @@
 import { get } from "./client.js";
 
-export async function fetchEvents() {
-  const res = await get("/api/events");
+export async function fetchEvents(filters = {}) {
+  const params = new URLSearchParams();
+
+  if (filters.title) {
+    params.set("title", filters.title);
+  }
+
+  if (filters.category) {
+    params.set("category", filters.category);
+  }
+
+  if (filters.status) {
+    params.set("status", filters.status);
+  }
+
+  if (filters.location) {
+    params.set("location", filters.location);
+  }
+
+  if (filters.organizer_id !== undefined) {
+    params.set("organizer_id", filters.organizer_id);
+  }
+
+  if (filters.startDate) {
+    params.set("startDate", filters.startDate);
+  }
+
+  if (filters.endDate) {
+    params.set("endDate", filters.endDate);
+  }
+
+  const query = params.toString();
+
+  const res = await get(`/api/events${query ? `?${query}` : ""}`);
+
   return res.data?.events || [];
 }
 
